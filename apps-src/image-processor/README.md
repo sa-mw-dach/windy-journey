6f87289dacb894281c00c32506dc3239339a684d
Stefan Bergstein <stefan.bergstein@gmail.com>
2023-11-15 19:58:44 +0100

    initial version of image processor

---
+++ apps-src/image-processor/README.md
@@ -0,0 +1,14 @@
+# Image processor for the Visual Inspections workshop
+
+
+## Build and test container image locally
+
+
+### Build
+```
+podman build -t image-processor -f Containerfile
+```
+
+### Test
+
+TO-DO: refactor test from manuela-visual-inspection
---
+++ apps-src/image-processor/image-processor.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import time
+import logging
+
+from flask import Flask, request, make_response
+from cloudevents.http import CloudEvent, to_structured, from_http
+
+import requests
+
+import uuid
+import cv2
+import json
+import base64
+import numpy as np
+
+from remote_infer_rest import ort_v5
+
+infer_url = None
+
+def convert_b64jpeg_to_image(b64jpeg):
+
+    # Decode base64 string in bytes
+    img_bytes = base64.b64decode(b64jpeg)
+  
+    # Convert in np array 
+    jpg_as_np = np.frombuffer(img_bytes, dtype=np.uint8)
+
+    # Decode into cv2 image
+    return cv2.imdecode(jpg_as_np, flags=1) 
+
+def convert_image_to_jpeg(image):
+    # Encode frame as jpeg
+    frame = cv2.imencode('.jpg', image)[1].tobytes()
+
+    # Encode frame in base64 representation and remove utf-8 encoding
+    frame = base64.b64encode(frame).decode('utf-8')
+    return "data:image/jpeg;base64,{}".format(frame)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def process_image():
+    # app.logger.debug(request.headers)
+
+    # create a CloudEvent
+    event = from_http(request.headers, request.get_data())
+
+    # you can access cloudevent fields as seen below
+    app.logger.info(
+        f"Found {event['id']} from {event['source']} with type "
+        f"{event['type']} and specversion {event['specversion']}"
+    )
+    # app.logger.info(event)
+
+    data = event.data
+
+    if 'image' in data and 'time' in data:
+
+        frame = convert_b64jpeg_to_image(data['image'].split(',')[1])
+
+        app.logger.info(data['time'] + " " + str(frame.shape))
+
+
+        # check / set cam ID
+        cam_id = 0
+        if 'id' in data:
+            cam_id = data['id']
+
+
+        # Call Model Mesh for object (damage) detection
+
+        # Define classes file
+        classes_file = 'data.yaml'
+
+        #  Set Confidence threshold, between 0 and 1 (detections with less score won't be retained)
+        conf = 0.2
+
+        #  Intersection over Union Threshold, between 0 and 1 (cleanup overlapping boxes)
+        iou = 0.4
+
+        # Inferencing
+        start = time.time()
+
+        infer=ort_v5(frame, infer_url, conf, iou, 640, classes_file)
+        try:
+            img, out, result = infer()
+            end = time.time()
+
+            app.logger.info('Predict: Total object detection took {:.5f} seconds'.format(end - start))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            # check if something was detected
+            if list(out.shape)[0] > 0:
+                status = 1
+            else:
+                status = 0
+
+            text = data['time']
+
+            # Respond with another event 
+            response = make_response({
+                'text': text,
+                'id': cam_id,
+                'status': status,
+                'image': convert_image_to_jpeg(img),
+            })
+            response.headers["Ce-Id"] = str(uuid.uuid4())
+            response.headers["Ce-specversion"] = "1.0"
+            response.headers["Ce-Source"] = "wvi/eventing/image-processor"
+            response.headers["Ce-Type"] = "wvi.image-processor.response"
+            
+
+
+        except BaseException as error:
+            app.logger.error("inferecing failed!")
+            app.logger.error("An exception occurred: {}".format(error)) 
+         
+         
+            response = make_response({
+                'msg': 'inferecing failed'
+            })
+            response.headers["Ce-Id"] = str(uuid.uuid4())
+            response.headers["Ce-specversion"] = "1.0"
+            response.headers["Ce-Source"] = "wvi/eventing/image-processor"
+            response.headers["Ce-Type"] = "wvi.image-processor.error"
+
+    else:
+        app.logger.warning("Payload not valid.")
+        response = make_response({
+            'msg': 'Payload not valid'
+        })
+        response.headers["Ce-Id"] = str(uuid.uuid4())
+        response.headers["Ce-specversion"] = "1.0"
+        response.headers["Ce-Source"] = "wvi/eventing/image-processor"
+        response.headers["Ce-Type"] = "wvi.image-processor.error"       
+    return response
+
+
+if __name__ == '__main__':
+
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.info("Start image processor ...")
+
+    infer_url = os.getenv("INFER_URL", default="https://wvi-vi-onnx-ods-project-stefan.apps.ocp5.stormshift.coe.muc.redhat.com/v2/models/wvi-vi-onnx/infer")
+
+    app.run(debug=False, host='0.0.0.0', port=8080)
